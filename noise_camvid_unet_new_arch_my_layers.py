097d17b851930aa3e28e6a71eb7de6ed3cb98d67
vaddevenkatesh19 <96912182+vaddevenkatesh19@users.noreply.github.com>
2024-04-18 09:15:04 +0530

    Update noise_camvid_unet_new_arch_my_layers.py

--- noise_camvid_unet_new_arch_my_layers.py
+++ noise_camvid_unet_new_arch_my_layers.py
@@ -6,17 +6,11 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
 import os
 for dirname, _, filenames in os.walk('camvid_dataset'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
         break
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 
 import matplotlib.pyplot as plt
