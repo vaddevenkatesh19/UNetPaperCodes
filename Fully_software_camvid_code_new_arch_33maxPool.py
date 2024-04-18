04c995036ba17527fbd190ce0181ae167102245a
vaddevenkatesh19 <96912182+vaddevenkatesh19@users.noreply.github.com>
2024-04-18 09:14:19 +0530

    Update Fully_software_camvid_code_new_arch_33maxPool.py

--- Fully_software_camvid_code_new_arch_33maxPool.py
+++ Fully_software_camvid_code_new_arch_33maxPool.py
@@ -2,27 +2,15 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-
-##### https://www.kaggle.com/code/mayankrajpurohit/camvid-segmentation-u-net/notebook
-
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
 for dirname, _, filenames in os.walk('camvid_dataset'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
         break
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 
 import matplotlib.pyplot as plt
